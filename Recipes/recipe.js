18a2fbec88ac4de068473ccfe916bee423b2ed2c
stephenroy <83079776+stephen2216@users.noreply.github.com>
2022-04-06 12:30:44 +0530

    Add files via upload

---
+++ Recipes/recipe.js
@@ -0,0 +1,139 @@
+var selectrecipe = document.getElementById("recipe")
+
+function maketea() {
+    selectrecipe.innerHTML = `
+    <h1>Ingrediants</h1>
+    <ol>
+    <li>1 cup (250 ml) Milk</li>
+    <li>2 teaspoons Tea Powder</li>
+    <li>1/4 cup (approx. 60 ml) Water</li>
+    <li>3 teaspoons Sugar</li>
+    </ol>
+    <br>
+    <h1>Steps:</h1>
+    <ol>
+    <li>Boil water in a saucepan.</li>
+    <li>Add sugar and tea powder in it and boil it for 3-4 minutes on medium flame.</li>
+    <li>Add milk and boil it over medium flame for 6-7 minutes or until  bubble starts to rise. You will see the change in color of the tea from milky shade to brown shade when it is ready.</li>
+    <li>Turn off the gas and strain tea in cups.
+    </li>
+    </ol>
+    `
+}
+
+function makecoffe() {
+    selectrecipe.innerHTML = `
+    <h1>Ingrediants</h1>
+    <ol>
+    <li>Coffee powder</li>
+    <li>Water</li>
+    <li>Milk</li>
+    <li>Sugar</li>
+    </ol>
+    <br>
+    <h1>Steps:</h1>
+    <ol>
+    <li>Mix 1 tsp of instant coffee per cup of hot water or hot water and milk solution.</li>
+    <li>Serve hot with desired amount of sugar.</li>
+    </ol>
+    `
+}
+function makegreentea() {
+    selectrecipe.innerHTML = `
+    <h1>Ingrediants</h1>
+    <ol>
+    <li>2 teaspoons Green tea leaves
+    </li>
+    <li>2 cups Water
+    </li>
+    <li>Honey , (optional) to taste
+    </li>
+    </ol>
+    <br>
+    <h1>Steps</h1>
+    <ol>
+    <li>To begin making Green Tea Recipe, take a saucepan and start heating water on a high heat.
+    </li>
+    <li>Once the water starts boiling, turn off the heat and add the green tea leaves.</li>
+    <li>Keep it covered for a minute.</li>
+    <li>Strain the green tea into the tea cups.</li>
+    <li>Add honey to taste (only as a sweetener, you can completely skip it), give it a swirl and serve.</li>
+    <li>Serve Green tea recipe with a snack like Baked Aloo Tikki Recipe and Dhaniya Pudina Chutney Recipe (Green Chutney) during evenings.</li>
+    </ol>
+    `
+}
+function makechickensoup() {
+    selectrecipe.innerHTML = `
+    <h1>Ingrediants</h1>
+    <ol>
+    <li>1 whole chicken (about 4 pounds), cut into pieces (including back)</li>
+    <li>8 cups water</li>
+    <li>Coarse salt</li>
+    <li>3 medium onions, thinly sliced (4 cups)</li>
+    <li>2 celery stalks, sliced crosswise 1/4 inch thick</li>
+    <li>4 garlic cloves, crushed</li>
+    <li>6 medium carrots, sliced 1/2 inch thick</li>
+    </ol>
+    <br>
+    <h1>Steps</h1>
+    <ol>
+    <li>Bring chicken, water, and 1 tablespoon salt to a boil in a large stockpot. Skim foam. Add onions, celery, and garlic. Reduce heat. Simmer, partially covered, for 30 minutes.</li>
+    <li>Remove breast, and set aside. Add carrots. Simmer, partially covered, for 40 minutes.</li>
+    <li>Remove remaining chicken; discard back and wings. Let cool slightly. Remove meat from bones, and cut into bite-size pieces.</li>
+    <li>Stir in desired amount of chicken; reserve the rest for another use. Skim fat. Season with salt.</li>
+    </ol>
+    `
+}
+function makegrapejuice() {
+    selectrecipe.innerHTML = `
+    <h1>Ingrediants</h1>
+    <ol>
+    <li>1 Cup black grapes</li>
+    <li>sugar (as per taste)</li>
+    <li>water (as required)</li>
+    <li>1 bowl ice water</li>
+    </ol>
+    <br>
+    <h1>Steps</h1>
+    <ol>
+    <li>Boil water with sugar and add grapes to it. Boil till the skin of the grapes gets cracked. Strain the water and keep aside.</li>
+    <li>Transfer the blanched grapes in ice water immediately to keep the colour intact.</li>
+    <li>De-skin the grapes and keep the skin aside. Refrigerate the pulp for at least an hour.</li>
+    <li>Now, boil the grape skin in grape water (that was kept aside) on medium to low flame for at least 15 minutes.</li>
+    <li>Strain the water and refrigerate till the juice turns chilled.6.Now, mix the pulp and juice and serve.</li>
+    </ol>
+    `
+}
+function makebabycornsoup() {
+    selectrecipe.innerHTML = `
+    <h1>Ingrediants</h1>
+    <ol>
+    <li>1 handful baby corn</li>
+    <li>4 cup vegetable bro</li>th</li>
+    <li>2 tablespoon low fat </li>butter</li>
+    <li>2 tablespoon corn</li>
+    <li>crushed black pepper as required</li>
+    <li>1 cup finely chopped onion</li>
+    <li>salt as required</li>
+    <li>2 tablespoon corn flour</li>
+    <li>2 tablespoon chopped spring onions</li>
+    </ol>
+    <br>
+    <h1>Steps</h1>
+    <ol>
+    <li>
+    Wash baby corns under running water and place them over a chopping board, slice them into pieces and keep aside.
+    </li>
+    <li>
+    Take a bow and mix together 1 cup of vegetable broth and corn flour, keep aside till required. Put a deep-bottomed pan over medium flame and melt butter in it. Add the finely chopped onions in the pan and saute the onions for about a minute or two.
+    </li>
+    <li>
+    Next, add the sliced baby corns, red bell pepper, chopped spring onions, carrot, and corns in the pan and saute for 2 minutes. Now, pour the remaining vegetable broth and stir well. Then, pour the corn flour mixture into the pan and stir continuously.
+    </li>
+    <li>
+    Season with salt and pepper as per your taste and cook for about 5-10 minutes. Turn off the burner when done and serve hot with your choice of bread.
+    </li>
+
+    </ol>
+    `
+}
